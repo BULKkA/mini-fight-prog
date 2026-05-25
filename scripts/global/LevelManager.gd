e9915266e9001130a4c351f4e6668695422f25b0
sergey.teplov <sergey.teplov@ddream.kz>
2026-05-25 16:03:42 +0500

    Добавил запуск левела

--- scripts/global/LevelManager.gd
+++ scripts/global/LevelManager.gd
@@ -1,18 +1,27 @@
 extends Node
 
-@onready var LevelsGrid: GridContainer = $LevelsGrid
+@onready var LevelsGrid: GridContainer = $CenterContainer/LevelsGrid
 @onready var levels = load("res://data/levels/Levels.tres")
 
 func _ready() -> void:
-	for level in levels:
+	for level in levels.data:
 		var button = Button.new()
 		button.text = level.name
 		button.pressed.connect(_on_level_pressed.bind(level.id))
 		LevelsGrid.add_child(button)
 
 func _on_level_pressed(level_id):
-	levels[level_id]
+	var level = levels.data[level_id]
+	OpenSceneWithData(level.link, level.level_data)
 
 
 func OpenSceneWithData(Scene, Data):
-	pass
+	var new_scene = load(Scene)
+	InitializeScene(new_scene, load(Data))
+	get_tree().change_scene_to_packed(new_scene)
+
+func InitializeScene(scene, data):
+	SpawnManager.set_scene(scene)
+	SpawnManager.SpawnPlayer(data.player_spawn_position)
+	
+	
--- scripts/global/SpawnManager.gd
+++ scripts/global/SpawnManager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+var current_scene
+var weaponSpawnPoints = []
+var enemySpawnPoints = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +16,12 @@ func SpawnWeapon(Weapon) -> void:
 
 func SpawnEnemy(Enemy) -> void:
 	pass
+
+func SpawnPlayer(position) -> void:
+	player = load("res://scenes/Player/Player.tscn").instance()
+	player.position = position
+	current_scene.add_child(player)
+
+func set_scene(scene):
+	current_scene = scene
+
